84d0233d2c522cfedeaceb49f0110bf635574681
ys4ber <youss42@proton.me>
2024-07-27 14:14:31 +0100

    ex00 Done ✅

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,34 +1,38 @@
 #include "Fixed.hpp"
 
-
 Fixed::Fixed()
 {
-    this->_value = 0;
     std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &other)
+Fixed::Fixed(const Fixed &n)
 {
     std::cout << "Copy constructor called" << std::endl;
-    *this = other;
+    setRawBits(n._value);
+    _value = getRawBits();
 }
 
-Fixed& Fixed::operator=(const Fixed &other)
+
+Fixed& Fixed::operator=(const Fixed &d)
 {
-    std::cout << "Assignation operator called" << std::endl;
-    this->_value = other.getRawBits();
+    std::cout << "Copy assignment operator called" << std::endl;
+    setRawBits(d._value);
+    _value = getRawBits();
     return *this;
 }
 
-int Fixed::getRawBits(void) const
+int Fixed::getRawBits() const
 {
     std::cout << "getRawBits member function called" << std::endl;
-    return this->_value;
+    return _value;
 }
 
+void Fixed::setRawBits(int const raw)
+{
+    _value = raw;
+}
 
 Fixed::~Fixed()
 {
     std::cout << "Destructor called" << std::endl;
 }
-
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -7,12 +7,13 @@ class Fixed
 {
     private:
         int _value;
-        static const int _fractionalBits = 8;
+        static const int _fractBits = 8;
 
     public:
         Fixed();
-        Fixed(const Fixed &other);            // Copy constructor
-        Fixed &operator=(const Fixed &other);
-        int getRawBits(void) const;
+        Fixed (const Fixed &n);
+        Fixed &operator=(const Fixed &d);
         ~Fixed();
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
 };
